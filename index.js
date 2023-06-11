10e85396e25d9b92922f388594b1b929ed7e06ee
sasha wangui <sasha.iluku@gmail.com>
2023-06-12 00:56:33 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+let introduction = name =>{
+    return(`Hi, my name is ${name}.`)
+}
+
+
+let introductionWithLanguage = (name,language="JavaScript") =>{
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+let introductionWithLanguageOptional =(name, language="JavaScript") =>{
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
